40b997bc5d4b3b613da070b87335f967df4c6edd
Jameson Lopp <jameson.lopp@gmail.com>
2018-01-01 16:31:23 -0500

    fix exahash math

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -222,7 +222,7 @@ UniValue getmininginfo(const JSONRPCRequest& request)
     obj.push_back(Pair("networkhashps",    getnetworkhashps(request)));
     obj.push_back(Pair("pooledtx",         (uint64_t)mempool.size()));
     obj.push_back(Pair("chain",            Params().NetworkIDString()));
-    statsClient.gauge("network.exahashesPerSecond", getnetworkhashps(request).get_real() / 10e18);
+    statsClient.gauge("network.exahashesPerSecond", getnetworkhashps(request).get_real() / 1e18);
     statsClient.gauge("network.difficulty", (double)GetDifficulty());
     return obj;
 }
