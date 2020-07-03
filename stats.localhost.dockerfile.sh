dd16019d6388d10d20bfa8f465c9ea1a2204b70c
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-07-03 19:41:28 -0400

    rename stats.localhost 	deleted:    localhost.dockerfile 	deleted:    localhost.dockerfile.sh
     Untracked files:
    	stats.localhost.dockerfile
    	stats.localhost.dockerfile.sh

--- stats.localhost.dockerfile.sh
+++ stats.localhost.dockerfile.sh
@@ -7,15 +7,14 @@ if [[ -d $HOME/stats.bitcoincore.dev ]];then
     cd $HOME/stats.bitcoincore.dev && git pull -f  origin +master:master
 else
     git clone --depth 1 https://github.com/bitcoincore-dev/stats.bitcoincore.dev.git  $HOME/stats.bitcoincore.dev
-
+fi
 cd $HOME/stats.bitcoincore.dev
 
-fi
 docker image rm -f stats.localhost
 
-docker build -f localhost.dockerfile --rm -t stats.localhost .
-#docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:80 -p 3000:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.localhost
+docker build -f stats.localhost.dockerfile --rm -t stats.localhost .
 #NOTE this assumes that the host isn't already using port 80
-#For example if you are using Digital Ocean - a droplet at stats.bitcoincore.dev will have the port available
 docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:3000 stats.localhost
+#ELSE use
+#docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 3000:3000 stats.localhost
 
