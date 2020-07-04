c3e835e82d0ac696db43bd069cde13a6998357a0
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-07-04 03:36:44 -0400

    remove /var/log/grafana creation

--- conf/run.grafana.sh
+++ conf/run.grafana.sh
@@ -77,13 +77,13 @@ if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
   done
 fi
 
-exec su-exec root grafana-server                            \
+exec su-exec grafana grafana-server                         \
   --homepath="$GF_PATHS_HOME"                               \
   --config="$GF_PATHS_CONFIG"                               \
   --packaging=docker                                        \
   "$@"                                                      \
   cfg:default.log.mode="console"                            \
   cfg:default.paths.data="$GF_PATHS_DATA"                   \
-  cfg:default.paths.logs="$GF_PATHS_LOGS"                   \
+#  cfg:default.paths.logs="$GF_PATHS_LOGS"                   \
   cfg:default.paths.plugins="$GF_PATHS_PLUGINS"             \
   cfg:default.paths.provisioning="$GF_PATHS_PROVISIONING"
