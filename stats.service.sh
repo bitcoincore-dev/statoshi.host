13ad638dd9b7f6187ef44f00f06925c590f865c4
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-07-05 15:41:27 -0400

    change public port to 80

--- stats.service.sh
+++ stats.service.sh
@@ -8,6 +8,7 @@ cat footer.dockerfile >> stats.bitcoincore.dev.dockerfile
 #docker image rm -f grafana.statsd.prebuild:latest
 #docker image rm -f stats.bitcoincore.dev:latest
 
-docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev . && docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:80 -p 3000:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
+#docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev . && docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:80 -p 3000:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
+docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev . && docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
 
 #docker builder prune -af
