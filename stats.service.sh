7a7280fdb0713503809ada43a4b5562d866d8b82
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-07-05 19:52:38 -0400

    extract working

--- stats.service.sh
+++ stats.service.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env bash
 TIME=$(date +%s)
 
+concat(){
 cat header.dockerfile > stats.bitcoincore.dev.dockerfile
 cat statoshi.dockerfile >> stats.bitcoincore.dev.dockerfile
 cat footer.dockerfile >> stats.bitcoincore.dev.dockerfile
+}
+build(){
+docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev .
+}
+extract(){
+rm -f $(pwd)/conf/usr/local/bin/bitcoin*
+#disable entrypoint
+sed '$d' stats.bitcoincore.dev.dockerfile | sed '$d' | sed '$d' >  stats.bitcoincore.dev.extract.dockerfile
+docker build -f stats.bitcoincore.dev.extract.dockerfile --rm -t statoshi.extract$TIME .
+docker image ls
+#docker run --name temp-container-name-$TIME statoshi.extract /bin/true &
+docker run --name statoshi.extract$TIME  statoshi.extract$TIME /bin/true
+docker cp statoshi.extract$TIME:/usr/local/bin/bitcoind        $(pwd)/conf/usr/local/bin/bitcoind
+docker cp statoshi.extract$TIME:/usr/local/bin/bitcoin-cli     $(pwd)/conf/usr/local/bin/bitcoin-cli
+docker cp statoshi.extract$TIME:/usr/local/bin/bitcoin-tx      $(pwd)/conf/usr/local/bin/bitcoin-tx
+docker rm statoshi.extract$TIME
 
-#docker image rm -f grafana.statsd.prebuild:latest
-#docker image rm -f stats.bitcoincore.dev:latest
-
-#docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev . && docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:80 -p 3000:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
-docker build -f stats.bitcoincore.dev.dockerfile --rm -t stats.bitcoincore.dev . && docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
-
-#docker builder prune -af
+rm -f  stats.bitcoincore.dev.extract.dockerfile
+}
+run(){
+docker run -e GF_AUTH_ANONYMOUS_ENABLED=true -it -p 80:3000 -p 8080:8080 -p 8125:8125 -p 8126:8126 stats.bitcoincore.dev .
+}
+concat
+build
+extract
+run
