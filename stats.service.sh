856eae6f9ff9f294bbcb13329a50d7223f958071
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-07-06 00:30:44 -0400

    fix entrypoint start bitcoind

--- stats.service.sh
+++ stats.service.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 TIME=$(date +%s)
 
+#DO NOT USE UNLESS YOU REALLY WANT TO DESTROY EVERY CONTAINER AND IMAGE ON THIS DROPLET
+#docker ps -aq && docker stop $(docker ps -aq) && docker rm $(docker ps -aq) && docker rmi $(docker images -q)
+
 concat(){
 cat header.dockerfile > stats.bitcoincore.dev.dockerfile
 cat statoshi.dockerfile >> stats.bitcoincore.dev.dockerfile
