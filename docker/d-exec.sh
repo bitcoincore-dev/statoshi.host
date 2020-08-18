677db74df3cc95c1c40d3c3756bb3f97fb7d57cb
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-08-18 15:00:01 -0400

    begin torproxy

---
+++ docker/d-exec.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Execute on the most recent composed image
+D_ID1=$(echo $(docker ps | awk '{print $1}' | awk 'NR==2'))
+echo $D_ID1
+echo "./docker/d-exec.sh"
+#docker exec -it $D_ID1 sh -c  "ls" \
+docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoind --prune=550"
+
