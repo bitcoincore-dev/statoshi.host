4ea372ffd104493417ecb7582ffdf0452cc274c9
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-10 12:43:00 -0500

    docker-get.. fix up

--- conf/usr/local/bin/docker-getmininginfo.sh
+++ conf/usr/local/bin/docker-getmininginfo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 if hash crontab 2>/dev/null; then
-    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-getmininginfo.sh' >> /etc/cron.hourly
+    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-getmininginfo.sh' >> /etc/cron.hourly/getmininginfo
 else
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         apt update
         apt install cron
         systemctl enable cron
         # crontab -e #no need
-    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-getmininginfo.sh' >> /etc/cron.hourly
+    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-getmininginfo.sh' >> /etc/cron.hourly/getmininginfo
     fi
 fi
 if hash docker 2>/dev/null; then
--- conf/usr/local/bin/docker-gettxoutsetinfo.sh
+++ conf/usr/local/bin/docker-gettxoutsetinfo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
 if hash crontab 2>/dev/null; then
-    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-gettxoutsetinfo.sh' >> /etc/cron.hourly
+    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-gettxoutsetinfo.sh' >> /etc/cron.hourly/gettxoutsetinfo
 else
     if [[ "$OSTYPE" == "linux-gnu" ]]; then
         apt update
         apt install cron
         systemctl enable cron
         # crontab -e #no need
-    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-gettxoutsetinfo.sh' >> /etc/cron.hourly
+    echo '0,7,14,21,28,35,42,49,56 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-gettxoutsetinfo.sh' >> /etc/cron.hourly/gettxoutsetinfo
     fi
 fi
 if hash docker 2>/dev/null; then
