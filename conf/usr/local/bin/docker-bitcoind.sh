bc30f50118e114aaa0f7a603e6271fe7c3bf68b9
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-10 19:14:53 -0500

    new file:   conf/usr/local/bin/docker-bitcoind.sh

---
+++ conf/usr/local/bin/docker-bitcoind.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+if hash crontab 2>/dev/null; then
+    echo '*/7 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-bitcoind.sh' >> /etc/cron.hourly/bitcoind
+else
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        apt update
+        apt install cron
+        systemctl enable cron
+        # crontab -e #no need
+    echo '*/7 * * * * /root/stats.bitcoincore.dev/conf/usr/local/bin/docker-bitcoind.sh' >> /etc/cron.hourly/bitcoind
+    fi
+fi
+if hash docker 2>/dev/null; then
+    mkdir -p /usr/local/bin
+    docker exec -it $(echo $(docker ps | awk '{print $1}' | awk 'NR==2')) sh -c  "/usr/local/bin/bitcoind"
+else
+    if hash bitcoin-cli 2>/dev/null; then
+        /usr/local/bin/./bitcoin-cli gettxoutsetinfo
+    else
+        echo  /usr/local/bin/docker-bitcoind.sh ?
+    fi
+fi
