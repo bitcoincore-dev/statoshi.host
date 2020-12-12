853853268ab090a7289f32a8a96e3a74a9234c19
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-11 19:34:19 -0500

    rename automate.sh

--- conf/usr/local/bin/stats-console.sh
+++ conf/usr/local/bin/stats-console.sh
@@ -3,7 +3,7 @@
 if hash docker 2>/dev/null; then
     mkdir -p /usr/local/bin
     #install -v /root/stats.bitcoincore.dev/conf/local/bin/docker-gettxoutsetinfo.sh /usr/local/bin/
-    docker exec -it $(echo $(docker ps | awk '{print $1}' | awk 'NR==2')) sh -c  "tail -f /home/root/.bitcoin/debug.log"
+    docker exec -it $(echo $(docker ps | awk '{print $1}' | awk 'NR==2')) sh -c  "tail -f $HOME/.bitcoin/debug.log"
 else
-        tail -f /home/root/.bitcoin/debig.log
+        watch tail -f $HOME/.bitcoin/debug.log
 fi
