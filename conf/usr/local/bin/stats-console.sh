d71052f8e7a58f7b5ce912a229711dafdf4b951e
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-18 00:00:14 -0500

    cp d-exec.sh to stats-console.sh

--- conf/usr/local/bin/stats-console.sh
+++ conf/usr/local/bin/stats-console.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-#
-REPO=$(find / -maxdepth 5 -type d -name stats.bitcoincore.dev 2>&1) | grep -v find
-export REPO
-DATADIR=$(find / -maxdepth 3 -type d -name .bitcoin 2>&1) | grep -v find
-export DATADIR
+# Execute on the most recent composed image
+D_ID1=$(echo $(docker ps | awk '{print $1}' | awk 'NR==2'))
+echo Container: $D_ID1
+docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoind --daemon" &
+docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoin-cli gettxoutsetinfo" &
+docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoin-cli getmininginfo" &
+docker exec -it $D_ID1 sh -c  "tail -f ~/.bitcoin/debug.log"
 
-if hash docker 2>/dev/null; then
-    mkdir -p /usr/local/bin
-    install -v $REPO/conf/local/bin/stats-console.sh /usr/local/bin/stats-console.sh
-    docker exec -it $(echo $(docker ps | awk '{print $1}' | awk 'NR==2')) sh -c  "tail -f $DATADIR/debug.log"
-else
-        watch tail -f $DATADIR/debug.log
-fi
