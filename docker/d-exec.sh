012cbea2f995d8fae1cd40b812a5d8c3bc8c984c
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-12-17 23:59:13 -0500

    modified:   docker/d-exec.sh

--- docker/d-exec.sh
+++ docker/d-exec.sh
@@ -4,5 +4,6 @@ D_ID1=$(echo $(docker ps | awk '{print $1}' | awk 'NR==2'))
 echo $D_ID1
 echo "./docker/d-exec.sh"
 #docker exec -it $D_ID1 sh -c  "ls" \
-docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoind --prune=550"
+#docker exec -dit $D_ID1 sh -c  "/usr/local/bin/bitcoind --daemon"
+docker exec -it $D_ID1 sh -c  "tail -f ~/.bitcoin/debug.log"
 
